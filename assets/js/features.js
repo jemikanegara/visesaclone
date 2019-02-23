0d04c0a78513c986c09d367843135c3f17cd852d
jemikanegara <jemikanegara@gmail.com>
2019-02-23 15:02:02 +0800

    html tag

--- assets/js/features.js
+++
@@ -1,5 +0,0 @@
-const featuresEl = document.getElementById("features");
-const feature = axios
-  .get("assets/js/features.json")
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
